2432e795c6b35009023f888bd87b9eb6cc97b5d4
69070285 <69070285@kmitl.ac.th>
2026-07-10 13:00:10 +0700

    problem

--- OverlapCircle.py
+++ OverlapCircle.py
@@ -1,9 +1,13 @@
 """OverlapCircle"""
-
+from math import sqrt
 x1 = int(input())
 y1 = int(input())
 r1 = int(input())
 x2 = int(input())
 y2 = int(input())
 r2 = int(input())
-
+d = sqrt(((x1-x2)**2)+((y1-y2)**2))
+if r1 > d/2 or r2 > d/2:
+    print("overlapping")
+else:
+    print("no overlapping")
